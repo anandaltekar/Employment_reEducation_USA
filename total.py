8dc56c7dd3126c5951c5a27c55d358c42843d760
Anand Altekar <anandaltekar@gmail.com>
2019-08-20 11:02:15 -0400

    Delete total.py

--- total.py
+++
@@ -1,8 +0,0 @@
-input = open("CES-Data.txt", "r")
-#out = open("output.txt", "w")
-series_ID = []
-
-for line in input:
-	line = line.strip().split("\t")
-	series_ID.append(list(line[0].strip()))
-print(len(series_ID))
